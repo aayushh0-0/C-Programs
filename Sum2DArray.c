a5e599f2613adf793b56170a033deff8d3d664aa
Ayush Rawat <ayushrawat24070@gmail.com>
2026-03-31 15:03:34 +0530

    Size Changes

--- Sum2DArray.c
+++ Sum2DArray.c
@@ -1,45 +1,48 @@
 #include<stdio.h>
 int main()
 {
+    int n;
     int a[10][10],b[10][10];
+    printf("Enter the Size of the Array\n");
+    scanf("%d",&n);
     printf("Enter the Elements in Matrix 1 ~\n");
-    for(int i=0;i<3;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<n;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
     printf("Enter the Elements in Matrix 2 ~\n");
-    for(int i=0;i<3;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<n;j++)
         {
             scanf("%d",&b[i][j]);
         }
     }
     printf("Matrix 1 is ~\n");
-    for(int i=0;i<3;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<n;j++)
         {
             printf("%d",a[i][j]);
         }
         printf("\n");
     }
     printf("Matrix 2 is ~\n");
-    for(int i=0;i<3;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<n;j++)
         {
             printf("%d",b[i][j]);
         }
         printf("\n");
     }
     printf("The New Array is ~ \n");
-    for(int i=0;i<3;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<n;j++)
         {
             printf("%d ",(a[i][j]+b[i][j]));
         }
